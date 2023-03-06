aafd2b024d262354e9f12236cdc0b46eb4263dfc
Sergei Martynov <Eldar231109@gmail.com>
2023-03-06 17:26:08 +0300

    router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,15 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/Home.vue'
+import OneArticle from "@/components/OneArticle";
+import EditComment from "@/components/EditComment";
+import EditingArticle from "@/components/EditingArticle";
+import CommentsList from "@/components/CommentsList";
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: Home
+    component: Home,
+    props: true
   },
   {
-
+    path: '/current/:id',
+    name: 'OneArticle',
+    component: OneArticle,
+    props: true
+  },
+  {
+    path: '/edit/:id/',
+    name: 'EditComment',
+    component: EditComment,
+    props: true
+  },
+  {
+    path: '/editArticle/:id/',
+    name: 'EditingArticle',
+    component: EditingArticle,
+    props: true
+  },
+  {
+    path: '/comments/',
+    name: 'CommentsList',
+    component: CommentsList,
+    props: true
   }
+
 ]
 
 const router = createRouter({
