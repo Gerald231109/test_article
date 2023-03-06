e3899aa765d1f58c8b02d55020899dd7aa741af4
Sergei Martynov <Eldar231109@gmail.com>
2023-03-06 17:09:04 +0300

    App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <v-app>
+    <v-app-bar color="teal-darken-4" :elevation="5">
+      <div class="d-flex align-center mr-2">
+        <v-btn color="white" to="/" class="mx-5">
+          Главная </v-btn>
+      </div>
+
+      <v-btn to="/comments/">
+        Комментарии</v-btn>
+    </v-app-bar>
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-}
+  export default {
+    name: "app",
+  };
 </script>
+
