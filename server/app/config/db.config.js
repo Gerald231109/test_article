667af5795071aa753680a1bb024ddeaa7be45721
Sergei Martynov <48458112+Gerald231109@users.noreply.github.com>
2023-03-06 18:13:52 +0300

    Update db.config.js

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     HOST: "localhost",
     USER: "postgres",
-    PASSWORD: "SergoKustNa100%Skiloviy",
-    DB: "test",
+    PASSWORD: "", // Здесь должен быть ваш пароль
+    DB: "test", // Здесь название БД
     dialect: "postgres",
     pool: {
         max: 5,
